66b3403433dfb7b8ee6d74c53399b641b070770f
nd <andriy.haluza@gmail.com>
2022-03-03 20:37:23 +0200

    feat: production server uri;

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,6 @@
 export const ApiConstants = {
-  serverApi: "http://185.233.119.173:3033/api/v1/",
+  // serverApi: "http://185.233.119.173:3033/api/v1/",
+  serverApi: "https://korabel.org.ua/api/v1/",
   accessTokenKey: "accessToken",
   refreshTokenKey: "refreshToken",
 };
